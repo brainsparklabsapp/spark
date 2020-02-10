f4e3217e5c6bec1235f913d44cc4d1b137e1083f
brainsparklabsapp <brainsparklabsapp@gmail.com>
2020-02-09 21:59:45 -0600

    Updates

---
+++ precache-manifest.32f8fdb54676d94ccc8ad4e9b56f7cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a60e9ff98f7ead39dcd09e625285fe2",
+    "url": "/spark/index.html"
+  },
+  {
+    "revision": "135f5e9957676621f108",
+    "url": "/spark/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "394c200fe7d03facf8de",
+    "url": "/spark/static/css/main.588a393e.chunk.css"
+  },
+  {
+    "revision": "135f5e9957676621f108",
+    "url": "/spark/static/js/2.050a8a04.chunk.js"
+  },
+  {
+    "revision": "394c200fe7d03facf8de",
+    "url": "/spark/static/js/main.6099c04a.chunk.js"
+  },
+  {
+    "revision": "478ff61932e6fdb08b55",
+    "url": "/spark/static/js/runtime-main.2e58b3f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6844fefb719c710fcf91471976f968f.js"
+  "/spark/precache-manifest.32f8fdb54676d94ccc8ad4e9b56f7cd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spark/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
