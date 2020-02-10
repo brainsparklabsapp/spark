e990024758884293f221ae5521e12d4b6a9a6e79
brainsparklabsapp <brainsparklabsapp@gmail.com>
2020-02-09 21:47:05 -0600

    Updates

---
+++ precache-manifest.4db9fab921657b8e3167790e018e303a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad0052477032b68b1fbf7e3a9a9cfe4",
+    "url": "/spark/index.html"
+  },
+  {
+    "revision": "135f5e9957676621f108",
+    "url": "/spark/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "edb043cf7e94ee47637b",
+    "url": "/spark/static/css/main.588a393e.chunk.css"
+  },
+  {
+    "revision": "135f5e9957676621f108",
+    "url": "/spark/static/js/2.050a8a04.chunk.js"
+  },
+  {
+    "revision": "edb043cf7e94ee47637b",
+    "url": "/spark/static/js/main.bf466681.chunk.js"
+  },
+  {
+    "revision": "478ff61932e6fdb08b55",
+    "url": "/spark/static/js/runtime-main.2e58b3f2.js"
+  }
+]);
