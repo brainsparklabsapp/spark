310b222518a5e1264b6e915141a7d22c44e8c02d
brainsparklabsapp <brainsparklabsapp@gmail.com>
2020-02-16 21:55:04 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ class App extends React.Component {
   render() {
     // For deploying to GitHub Pages, need to set basename of HashRouter
 
-// console.log("Process.env.PUBLIC_URL check");
-// if (process && process.env ) {
-  // console.log("Check PUBLIC_URL");
-// }
-// if (process && process.env && process.env.PUBLIC_URL) {
-  // console.log("Process.env.PUBLIC_URL: |" + process.env.PUBLIC_URL + "|");
-// }
+ console.log("Process.env.PUBLIC_URL check");
+ if (process && process.env ) {
+   console.log("Check PUBLIC_URL");
+ }
+ if (process && process.env && process.env.PUBLIC_URL) {
+   console.log("Process.env.PUBLIC_URL: |" + process.env.PUBLIC_URL + "|");
+ }
     return (
       <div className="App">
         <HashRouter basename="/">
